0f587341c56fac06472feccbf67d5543a4f05f9a
stargroot5 <iharel08@gmail.com>
2024-01-26 11:08:56 +0200

    the file was added

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
     public static void main(String[] args){
         for( int i=1; i<=10;i++){
-            System.out.println(i+": Hello, welcome my man!");
+            System.out.println(i+": new change!");
         }
     }
 }
